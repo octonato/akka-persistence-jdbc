e0a029cf10813a6467d33e56fe6dd777a0af77b0
Dennis Vriend <dnvriend@gmail.com>
2016-07-18 05:46:41 +0200

    docker for 12c

--- src/main/resources/schema/oracle/oracle-schema.sql
+++ src/main/resources/schema/oracle/oracle-schema.sql
@@ -1,26 +1,11 @@
-CREATE SEQUENCE "ordering_seq" start with 1 increment by 1 NOMAXVALUE;
-
 CREATE TABLE "journal" (
   "ordering" NUMERIC,
-  "deleted" char check ("deleted" in (0,1)),
   "persistence_id" VARCHAR(255) NOT NULL,
   "sequence_number" NUMERIC NOT NULL,
-  "created" NUMERIC NOT NULL,
+  "deleted" char check ("deleted" in (0,1)),
   "tags" VARCHAR(255) DEFAULT NULL,
   "message" BLOB NOT NULL,
   PRIMARY KEY("ordering", "persistence_id", "sequence_number")
-);
-
-CREATE TRIGGER "ordering_seq_trigger"
-BEFORE INSERT ON "journal"
-FOR EACH ROW
-BEGIN
-  select ordering_seq.nextval into :new.ordering from dual;
-END;
-
-CREATE TABLE "deleted_to" (
-  "persistence_id" VARCHAR(255) NOT NULL,
-  "deleted_to" NUMERIC NOT NULL
 );
 
 CREATE TABLE "snapshot" (
